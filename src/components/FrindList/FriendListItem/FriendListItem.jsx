b2dc3601c6fd2745b0ef17c670ba4e31d57313b8
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-10-25 17:52:41 +0300

    fixed span

--- src/components/FrindList/FriendListItem/FriendListItem.jsx
+++ src/components/FrindList/FriendListItem/FriendListItem.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 export const FriendListItem = ({friend}) => {
     return ( <li className={css.item} >
-        <span className="status">{friend.isOnline ? <span className={css.true}></span> : <span className={css.false}></span>}</span>
+        <span className={`status ${friend.isOnline ? css.true : css.false}`} />
         <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
         <p className={css.name}>{friend.name}</p>
         </li>)
